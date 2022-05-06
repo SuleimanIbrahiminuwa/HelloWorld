86938213d7a05d5f38799b46dc0a1f44897eea4b
Suleiman Ibrahim <isuleimanibrahim@gmail.com>
2022-05-06 22:25:14 +0100

    My first Commit

---
+++ src/main/java/org/example/practice.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class practice {
+    public static void main (String[] args){
+        int tem = 30;
+        if (tem > 30)
+            System.out.println("Its a hot day");
+
+    }
+
+
+
+
+}
